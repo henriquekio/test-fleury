9ade4f3c414dcfc7f333d3a03a033019e2af54b0
Henrique <henriquekio@hotmail.com>
2019-11-13 19:26:58 -0300

    Hotfix\Change style thumbnail in services list

--- src/views/Services/components/ServiceItem.js
+++ src/views/Services/components/ServiceItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const ServiceItem = (props) => {
+const ServiceItem = props => {
   const { service } = props;
   return (
     <div className="services">
-      <div className="services__image">
-        <img src={service.thumbnail} className="responsive-img" alt=""/>
-      </div>
+      <div
+        className="services__image"
+        style={{ backgroundImage: `url('${service.thumbnail}')` }}
+       />
       <div className="services__description">
         <h5>{service.title}</h5>
         <p>{service.subtitle}</p>
